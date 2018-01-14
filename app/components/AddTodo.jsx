4ba9ed1fc2c5c8666d6b767aad1d9af1b982da76
Roger Hardin <volcivar@yahoo.com>
2018-01-14 18:51:34 -0500

    style todo app

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -15,10 +15,10 @@ var AddTodo = React.createClass({
   },
   render: function () {
     return (
-      <div>
+      <div className="container__footer">
         <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="todoText" />
-          <button className="button expanded hollow">Add Todo</button>
+          <button className="button expanded">Add Todo</button>
         </form>
       </div>
     )
--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,25 +4,30 @@ var moment = require('moment');
 var Todo = React.createClass({
   render: function () {
     var {id, text, completed, createdAt, completedAt} = this.props;
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
       var message = 'Created ';
       var timestamp = createdAt;
 
       if (completed) {
         var message = 'Completed ';
-        var timestamp = completedAt;        
+        var timestamp = completedAt;
       }
 
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a')
     };
 
     return (
-      <div onClick={() => {
-        this.props.onToggle(id);
-      }}>
-        <input type="checkbox" checked={completed}></input>
-        <p>{text}</p>
-        <p>{renderDate()}</p>
+      <div className={todoClassName} onClick={() => {
+          this.props.onToggle(id);
+        }}>
+        <div>
+          <input type="checkbox" checked={completed}></input>
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
       </div>
     )
   }
