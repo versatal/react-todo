dc61ed077a0e0d82826c3cbd36185b0c583819db
Roger Hardin <volcivar@yahoo.com>
2018-01-14 06:35:15 -0500

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -15,6 +15,34 @@ module.exports = {
       todos = JSON.parse(stringTodos);
     } catch (e) {}
 
-    return $.isArray(todos) ? todos : [];    
+    return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filer by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filer by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+
+    //sort todos with nonCompleted first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+
+    return filteredTodos;
   }
 };
