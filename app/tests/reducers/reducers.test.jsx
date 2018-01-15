3fe5a828b2a34725cb1293673fca1ed37045f49b
Roger Hardin <volcivar@yahoo.com>
2018-01-15 16:34:39 -0500

    add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,40 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    //define todos array with realist todo item
+    //generate action
+    //call reducer and asert completed flipped
+    it('should toggle todo completed status', () => {
+      var testTodos = [
+        {
+          id: 123,
+          text: 'Go Running',
+          completed: true,
+          createdAt: 101010,
+          completedAt: 202020
+        }
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 123
+      };
+      var res = reducers.todosReducer(df(testTodos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+
+  });
 });
