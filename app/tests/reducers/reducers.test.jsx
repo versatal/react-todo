ed821bfc9d123e792f578233741e9be37fc75b12
Roger Hardin <volcivar@yahoo.com>
2018-01-15 15:38:47 -0500

    Add showCompleted and searchText reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,28 @@
+var expect = require('expect');
+var reducers = require('reducers');
+var df = require('deep-freeze-strict')
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+    // Test that the showCompleted status gets flipped
+  });
+  describe('showCompletedReducer', () => {
+    it('should toggle showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(res).toEqual(true);
+    });
+  });
+});
