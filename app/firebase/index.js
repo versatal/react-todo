f802317373e77916b9e66dc358252533f739cda1
Roger Hardin <volcivar@yahoo.com>
2018-01-19 00:12:29 -0500

    Setting up dual databases

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyBOzChqaHOYUinZReOM-JQH3d6yVxU_bYI",
-    authDomain: "hardin-todo-app.firebaseapp.com",
-    databaseURL: "https://hardin-todo-app.firebaseio.com",
-    projectId: "hardin-todo-app",
-    storageBucket: "hardin-todo-app.appspot.com",
-    messagingSenderId: "568812591650"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
