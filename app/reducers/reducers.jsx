8facba58f153f320d1c08735159d5d38f96ed4e3
Roger Hardin <volcivar@yahoo.com>
2018-01-21 08:18:38 -0500

    wipe todos on logout

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -43,6 +43,8 @@ export var todosReducer = (state = [], action) => {
         ...state,
         ...action.todos
       ];
+    case 'LOGOUT':
+      return [];
     default:
       return state;
   }
