49fbad33a5864d4e468b61e6f7ab6980f6051760
Roger Hardin <volcivar@yahoo.com>
2018-01-21 06:36:42 -0500

    update todos location to include uid

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -7,7 +7,7 @@ export var Login = React.createClass({
   onLogin() {
     var {dispatch} = this.props;
 
-    dispatch(actions.startLogin());
+    dispatch(actions.startogin());
   },
   render: function () {
     return (
